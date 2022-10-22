3518b5a1fa9ce238fce57d5ce326962a0fbdaec3
student <student@info-18.cshl.edu>
2022-10-22 19:51:46 -0400

    has naming, just needs variable tuning

--- userinput.py
+++ userinput.py
@@ -9,6 +9,8 @@ displaysurface.fill((255,255,255))
 basefont = pygame.font.Font(None, 28)
 
 inputdict = {'globrain':0.0, 'numtechplates':0, 'waterlev':0.0, 'globtemp':0.0}
+planetname = ''
+nameactive = False
 
 rainx = 100
 techx = 450
@@ -29,49 +31,64 @@ while True:
                  inputdict['waterlev'] = waterx-800
                  inputdict['globtemp'] = tempx-1150
                  print(inputdict) #trigger movement to next step here
+                 print(planetname)
+            elif nameactive:
+                 if event.key == pygame.K_BACKSPACE:
+                     planetname = planetname[:-1]
+                 else:
+                     planetname += event.unicode
 
     displaysurface.fill((255,255,255))
+
+    namerect = pygame.Rect(300, 700, 500, 30)
+    pygame.draw.rect(displaysurface, (200,200,200), namerect)
+    displaysurface.blit(basefont.render(planetname, True, (0,0,0)), (305, 705))
+    displaysurface.blit(basefont.render('Enter planet name:', True, (0,0,0)), (100, 705))
 
     if rainx >350:
         rainx = 350
     elif rainx <100:
         rainx = 100
-    rainrect = pygame.Rect(rainx, 400, 30, 30)
+    rainrect = pygame.Rect(rainx, 500, 30, 30)
     if techx >700:
         techx = 700
     elif techx <450:
         techx = 450
-    techrect = pygame.Rect(techx, 400, 30, 30)
+    techrect = pygame.Rect(techx, 500, 30, 30)
     if waterx >1050:
         waterx = 1050
     elif waterx <800:
         waterx = 800
-    waterrect = pygame.Rect(waterx, 400, 30, 30)
+    waterrect = pygame.Rect(waterx, 500, 30, 30)
     if tempx >1400:
         tempx = 1400
     elif tempx <1150:
         tempx = 1150
-    temprect = pygame.Rect(tempx, 400, 30, 30)
+    temprect = pygame.Rect(tempx, 500, 30, 30)
 
     if pygame.mouse.get_pressed()==(True,False,False):
         mousepos = pygame.mouse.get_pos()
         if rainrect.collidepoint(mousepos):
             rainx = mousepos[0]-15
-            rainrect.move(rainx, 400)
+            rainrect.move(rainx, 500)
         if techrect.collidepoint(mousepos):
             techx = mousepos[0]-15
-            techrect.move(techx,400)
+            techrect.move(techx,500)
         if waterrect.collidepoint(mousepos):
             waterx = mousepos[0]-15
-            waterrect.move(waterx,400)
+            waterrect.move(waterx,500)
         if temprect.collidepoint(mousepos):
             tempx = mousepos[0]-15
-            temprect.move(tempx,400)
+            temprect.move(tempx,500)
+        if namerect.collidepoint(mousepos):
+            nameactive = True
+        else:
+            nameactive = False
 
-    pygame.draw.rect(displaysurface, (100,100,100), pygame.Rect(100,410,280,10))
-    pygame.draw.rect(displaysurface, (100,100,100), pygame.Rect(450,410,280,10))
-    pygame.draw.rect(displaysurface, (100,100,100), pygame.Rect(800,410,280,10))
-    pygame.draw.rect(displaysurface, (100,100,100), pygame.Rect(1150,410,280,10))
+    pygame.draw.rect(displaysurface, (100,100,100), pygame.Rect(100,510,280,10))
+    pygame.draw.rect(displaysurface, (100,100,100), pygame.Rect(450,510,280,10))
+    pygame.draw.rect(displaysurface, (100,100,100), pygame.Rect(800,510,280,10))
+    pygame.draw.rect(displaysurface, (100,100,100), pygame.Rect(1150,510,280,10))
 
     pygame.draw.rect(displaysurface, (0,0,0), rainrect)
     pygame.draw.rect(displaysurface, (0,0,0), techrect)
@@ -87,15 +104,17 @@ while True:
     temptext = basefont.render('Enter Average Global Temperature', True, (0,0,0))
     tempval = basefont.render(str(tempx-1150)+' C', True, (0,0,0))
     entmess = basefont.render('Hit return to proceed', True, (0,0,0))
-    displaysurface.blit(raintext, (100,50))
-    displaysurface.blit(rainval, (150,200))
-    displaysurface.blit(techtext, (450,50))
-    displaysurface.blit(techval, (500,200))
-    displaysurface.blit(watertext, (850,50))
-    displaysurface.blit(waterval, (850,200))
-    displaysurface.blit(temptext, (1150,50))
-    displaysurface.blit(tempval, (1250,200))
-    displaysurface.blit(entmess, (600, 600))
+    displaysurface.blit(raintext, (100,150))
+    displaysurface.blit(rainval, (150,300))
+    displaysurface.blit(techtext, (450,150))
+    displaysurface.blit(techval, (500,300))
+    displaysurface.blit(watertext, (850,150))
+    displaysurface.blit(waterval, (850,300))
+    displaysurface.blit(temptext, (1150,150))
+    displaysurface.blit(tempval, (1250,300))
+    displaysurface.blit(entmess, (950, 700))
+    welcometext = pygame.font.Font(None,40).render('Welcome to La Croix!', True, (0,0,0))
+    displaysurface.blit(welcometext, (600,50))
 
     pygame.display.flip()
 
