c2f72ef9c7002abcd30bd615f24e9641fa1754d7
student <student@info-21.cshl.edu>
2022-10-24 12:04:03 -0400

    speedy biome to color

--- biome_to_color.py
+++ biome_to_color.py
@@ -41,37 +41,36 @@ def biome_colors(biomes_df):
 	for i in range(biomes_df.shape[0]):
 		for j in range(biomes_df.shape[1]): 
 			value1 = biomes_df.at[i,j]
-			for value in biomes_df: 
-				if value1 == 'desert': 
-#					biomes_df = biomes_df.replace(to_replace=['desert'], value=['(255,215,0)'])
-					biomes_df.loc[i].at[j] = (245,222,179)
-				elif value1 == 'grassland': 
-#					biomes_df = biomes_df.replace(to_replace=['grassland'], value=['(218,165,32)'])
-					biomes_df.loc[i].at[j] = (218,165,32)
-				elif value1 == 'tundra': 
-#					biomes_df = biomes_df.replace(to_replace=['tundra'], value=['(189,183,107)'])
-					biomes_df.loc[i].at[j] = (189,183,107)
-				elif value1 == 'rain_forest': 
-#					biomes_df = biomes_df.replace(to_replace=['rain_forest'], value=['(34,139,34)'])
-					biomes_df.loc[i].at[j] = (34,139,34)
-				elif value1 == 'temp_forest': 
-#					biomes_df = biomes_df.replace(to_replace=['temp_forest'], value=['(154,205,50)'])
-					biomes_df.loc[i].at[j] = (154,205,50)
-				elif value1 == 'taiga': 
-#					biomes_df = biomes_df.replace(to_replace=['taiga'], value=['(85,107,47)'])
-					biomes_df.loc[i].at[j] = (85,107,47)
-				elif value1 == 'polar': 
-#					biomes_df = biomes_df.replace(to_replace=['polar'], value=['(250,250,250)'])
-					biomes_df.loc[i].at[j] = (250,250,250)
-				elif value1 == 'frozen_water': 
-#					biomes_df = biomes_df.replace(to_replace=['frozen_water'], value=['(244,255,255)'])
-					biomes_df.loc[i].at[j] = (244,255,255)
-				elif value1 == 'cold_water': 
-#					biomes_df = biomes_df.replace(to_replace=['cold_water'], value=['(135,206,250)'])
-					biomes_df.loc[i].at[j] = (135,206,250)
-				elif value1 == 'med_water':
- 					biomes_df.loc[i].at[j] = (0,0,255)
-#					biomes_df = biomes_df.replace(to_replace=['med_water'], value='(0,0,255)')
+			if value1 == 'desert': 
+#				biomes_df = biomes_df.replace(to_replace=['desert'], value=['(255,215,0)'])
+				biomes_df.loc[i].at[j] = (245,222,179)
+			elif value1 == 'grassland': 
+#				biomes_df = biomes_df.replace(to_replace=['grassland'], value=['(218,165,32)'])
+				biomes_df.loc[i].at[j] = (218,165,32)
+			elif value1 == 'tundra': 
+#				biomes_df = biomes_df.replace(to_replace=['tundra'], value=['(189,183,107)'])
+				biomes_df.loc[i].at[j] = (189,183,107)
+			elif value1 == 'rain_forest': 
+#				biomes_df = biomes_df.replace(to_replace=['rain_forest'], value=['(34,139,34)'])
+				biomes_df.loc[i].at[j] = (34,139,34)
+			elif value1 == 'temp_forest': 
+#				biomes_df = biomes_df.replace(to_replace=['temp_forest'], value=['(154,205,50)'])
+				biomes_df.loc[i].at[j] = (154,205,50)
+			elif value1 == 'taiga': 
+#				biomes_df = biomes_df.replace(to_replace=['taiga'], value=['(85,107,47)'])
+				biomes_df.loc[i].at[j] = (85,107,47)
+			elif value1 == 'polar': 
+#				biomes_df = biomes_df.replace(to_replace=['polar'], value=['(250,250,250)'])
+				biomes_df.loc[i].at[j] = (250,250,250)
+			elif value1 == 'frozen_water': 
+#				biomes_df = biomes_df.replace(to_replace=['frozen_water'], value=['(244,255,255)'])
+				biomes_df.loc[i].at[j] = (244,255,255)
+			elif value1 == 'cold_water': 
+#				biomes_df = biomes_df.replace(to_replace=['cold_water'], value=['(135,206,250)'])
+				biomes_df.loc[i].at[j] = (135,206,250)
+			elif value1 == 'med_water':
+ 				biomes_df.loc[i].at[j] = (0,0,255)
+#				biomes_df = biomes_df.replace(to_replace=['med_water'], value='(0,0,255)')
 
 
 #print(biomes_df,'\n') 
